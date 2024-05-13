e02cebc1d14141a6f65bf28e2b2219985d074d5a
Justin Diamond <justdiam12@gmail.com>
2024-05-13 12:27:25 -0700

    Working

--- simulate.py
+++ simulate.py
@@ -36,11 +36,10 @@ class SBP_Simulate():
         self.pulse(index, up_down, time, coeff)
 
     def pulse(self, index, up_down, time, coeff):
-        if up_down == -1:
+        if up_down == -1: # Descending Pulse
             if index+1 == len(self.profile):
                 return
             else:
-                print("Descending pulse")
                 while self.impedance[index] == self.impedance[index+1]:
                     time += self.m_per_p / self.c_profile[index]
                     index += 1
@@ -53,36 +52,37 @@ class SBP_Simulate():
                 if T >= self.thresh:
                     self.pulse(index+1, -1, time, T)
 
-        elif up_down == 1:
+        elif up_down == 1: # Ascending Pulse
             if index-1 == -1:
                 return
             else:
-                print("Ascending pulse")
                 while self.impedance[index] == self.impedance[index-1]:
                     time += self.m_per_p / self.c_profile[index]
                     index -= 1
                     if index-1 == -1:
+                        time += self.m_per_p / self.c_profile[index]
                         self.twtt = np.append(self.twtt, time)
                         self.amp = np.append(self.amp, coeff)
                         return
                 R = ((self.impedance[index-1] - self.impedance[index]) / (self.impedance[index-1] + self.impedance[index])) * coeff
                 T = ((2*self.impedance[index-1]) / (self.impedance[index-1] + self.impedance[index])) * coeff
+                if T >= self.thresh:
+                    self.pulse(index-1, 1, time, T)
                 if R >= self.thresh:
                     self.pulse(index, -1, time, R)
-                if T >= self.thresh:
-                    self.pulse(index+1, 1, time, T)
 
 
 def run():
-    layers = [10, 50, 60]
+    layers = [10, 15, 20]
     rho = [1026, 1906, 1922, 1955]
     c = [1500, 1600, 1650, 1660]
-    thresh = 0.0001
+    thresh = 0.000001
     m_per_p = 1 # Meters/Pixel
     sbp_simulate = SBP_Simulate(layers, rho, c, m_per_p, thresh)
     sbp_simulate.simulate() 
+    print(sbp_simulate.twtt)
 
-    plt.plot(sbp_simulate.twtt, sbp_simulate.amp, "-o")
+    plt.plot(sbp_simulate.twtt, 20*np.log(sbp_simulate.amp), "o")
     plt.show()
 
 
