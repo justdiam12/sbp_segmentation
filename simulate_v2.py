5a2e79d5cc05f0152ddfe9a4a3cfc4c3f25c3bba
Justin Diamond <justdiam12@gmail.com>
2024-09-25 10:58:57 -0700

    Fixing recursion depth error

--- simulate_v2.py
+++ simulate_v2.py
@@ -16,8 +16,8 @@ class SBP_Simulate_v2():
         self.attenuation = attenuation # Attenuation in each layer (dB/m)
         self.sample_f = sample_f # Sonar sampling frequency (Hz)
         self.time_interval = time_interval # Frequency of the Sonar System (Hz)
-        self.pulse_width = 1 / self.sample_f # Time between sonar pulses (s)
-        self.size = self.time * self.sample_f # Length of the trackline 
+        self.pulse_width = 0.1 # Time between sonar pulses (s)
+        self.size = int(self.time * self.sample_f) # Length of the trackline 
         self.layers = random_contour(layers, self.size) 
 
         # Seafloor parameter profiles
@@ -25,25 +25,9 @@ class SBP_Simulate_v2():
         self.c_profile = np.zeros((len(self.c), self.size))
         self.attenuation_profile = np.zeros((len(self.attenuation), self.size))
         for j in range(self.size):
-            for i in range(self.layers.shape[0]):
-                layer_1 = int(self.layers[i,j])
-                if i == 0:
-                    layer_2 = int(self.layers[i+1,j])
-                    self.rho_profile[0:layer_1, j] = self.rho[i]
-                    self.c_profile[0:layer_1, j] = self.c[i]
-                    self.attenuation_profile[0:layer_1, j] = self.attenuation[i]
-                    self.rho_profile[layer_1:layer_2, j] = self.rho[i+1]
-                    self.c_profile[layer_1:layer_2, j] = self.c[i+1]
-                    self.attenuation_profile[layer_1:layer_2, j] = self.attenuation[i+1]
-                elif i == self.layers.shape[0]-1:
-                    self.rho_profile[layer_1:, j] = self.rho[i+1]
-                    self.c_profile[layer_1:, j] = self.c[i+1]
-                    self.attenuation_profile[layer_1:, j] = self.attenuation[i+1]
-                else:
-                    layer_2 = int(self.layers[i+1,j])
-                    self.rho_profile[layer_1:layer_2, j] = self.rho[i+1]
-                    self.c_profile[layer_1:layer_2,j] = self.c[i+1]
-                    self.attenuation_profile[layer_1:layer_2,j] = self.attenuation[i+1]
+            self.rho_profile[:,j] = self.rho
+            self.c_profile[:,j] = self.c
+            self.attenuation_profile[:,j] = self.rho
         self.impedance = self.rho_profile * self.c_profile
 
         # TWTT and Amplitude arrays
@@ -53,61 +37,88 @@ class SBP_Simulate_v2():
     def simulate(self):
         layer = 0
         up_down = -1 
-        t_0 = 0
-        t_layer = 0
-        dist = 0
+        t_elapsed = 0
+        t_in_layer = 0
+        dist_traveled = 0
+        dist_in_layer = 0
         coeff = 1
         rt = []
         for j in range(self.size):
-            self.pulse(layer, j, up_down, t_0, t_layer, dist, coeff, rt)
+            self.pulse(layer, j, up_down, t_elapsed, t_in_layer, dist_traveled, dist_in_layer, coeff, rt)
             
 
-    def pulse(self, layer, dim, up_down, time, t_layer, dist, coeff, rt):
+    def pulse(self, layer, dim, up_down, time_elapsed, t_in_layer, dist_traveled, dist_in_layer, coeff, rt):
         if up_down == -1: # Descending Pulse
-            if time + self.time_interval >= self.pulse_width:
+            if layer + 1 > self.layers.shape[0]:
                 return
             else:
-                while dist < self.layers[layer, dim]:
-                    time += self.time_interval
-                    t_layer += self.time_interval
-                    dist += self.c_profile[layer, dim] * self.time_interval
-                    if time + self.time_interval >= self.pulse_width:
-                        return
-                R = ((self.impedance[layer+1, dim] - self.impedance[layer, dim]) / (self.impedance[layer+1, dim] + self.impedance[layer, dim]))*coeff
-                T = ((2*self.impedance[layer+1, dim]) / (self.impedance[layer+1, dim] + self.impedance[layer, dim]))*coeff
-                self.pulse(layer+1, dist + 1, dim, 1, time + self.m_per_p / self.c_profile[index, dim], R, np.append(rt,R))
-                if layer+1 < len(self.layers.shape[0]):
-                    self.pulse(layer+1, dist, dim, -1, time, T, np.append(rt,T))
+                while dist_in_layer < self.layers[layer, dim] and time_elapsed < self.pulse_width:
+                    time_elapsed += self.time_interval
+                    t_in_layer += self.time_interval
+                    dist_traveled += self.c_profile[layer, dim] * self.time_interval
+                    dist_in_layer += self.c_profile[layer, dim] * self.time_interval
+                
+                # Fix for overshoot in distance
+                dist_difference = self.layers[layer, dim] - dist_in_layer
+                dist_traveled += dist_difference
+                dist_in_layer += dist_difference
+                time_difference = dist_difference / self.c_profile[layer, dim]
+                time_elapsed += time_difference
+                t_in_layer += time_difference
+                
+                # Compute R and T coefficients and recursive pulse call
+                R = ((self.impedance[layer+1, dim] - self.impedance[layer, dim]) / (self.impedance[layer+1, dim] + self.impedance[layer, dim])) * coeff
+                T = ((2*self.impedance[layer+1, dim]) / (self.impedance[layer+1, dim] + self.impedance[layer, dim])) * coeff
+                self.pulse(layer-1, dim, 1, time_elapsed, 0, dist_traveled, 0, R, np.append(rt,R))
+                if layer+1 < self.layers.shape[0]:
+                    self.pulse(layer+1, dim, -1, time_elapsed, 0, dist_traveled, 0, T, np.append(rt,T))
 
         elif up_down == 1: # Ascending Pulse
-            if layer == 0:
-                while t_layer < self.layers[layer, dim]/self.c_profile[layer, dim] and time < self.pulse_width:
-                    time += self.time_interval
-                    t_layer += self.time_interval
-                    dist += self.c_profile[layer, dim] * self.time_interval
-                    if time + self.time_interval >= self.pulse_width:
-                        return
-                index = np.where(self.twtt == time)
+            if layer == -1:
+                layer = 0
+                while dist_in_layer < self.layers[layer, dim] and time_elapsed < self.pulse_width:
+                    time_elapsed += self.time_interval
+                    t_in_layer += self.time_interval
+                    dist_traveled += self.c_profile[layer, dim] * self.time_interval
+                    dist_in_layer += self.c_profile[layer, dim] * self.time_interval
+                    
+                # Fix for overshoot in distance
+                dist_difference = self.layers[layer, dim] - dist_in_layer
+                dist_traveled += dist_difference
+                dist_in_layer += dist_difference
+                time_difference = dist_difference / self.c_profile[layer, dim]
+                time_elapsed += time_difference
+                t_in_layer += time_difference
+
+                # Append the amplitude information
+                index = np.where(self.twtt == time_elapsed)
                 self.amp[index, dim] = coeff
+                print(rt)
+
             else:
-                while self.impedance[index, dim] == self.impedance[index-1, dim]:
-                    time += self.m_per_p / self.c_profile[index, dim]
-                    index -= 1
-                    dist += 1
-                    if index-1 == -1:
-                        time += self.m_per_p / self.c_profile[index, dim]
-                        dist += 1
-                        if len(np.where(np.isclose(self.twtt, time, atol=1/self.frequency))[0]) > 0:
-                            inde = np.where(np.isclose(self.twtt, time, atol=1/self.frequency))[0]
-                            self.amp[dimes[0], dim] = coeff/dist
-                            self.amp_indices.append((int(np.round((dimes[0]/self.twtt.shape[0])*(self.size-1))), coeff/dist))
-                        return
-                R = ((self.impedance[index-1, dim] - self.impedance[index, dim]) / (self.impedance[index-1, dim] + self.impedance[index, dim])) * coeff
-                T = ((2*self.impedance[index-1, dim]) / (self.impedance[index-1, dim] + self.impedance[index, dim])) * coeff
-                if np.abs(T) >= self.thresh:
-                    self.pulse(index-1, dist + 1, dim, 1, time + self.m_per_p / self.c_profile[index, dim], T, np.append(rt,T))
-                if np.abs(R) >= self.thresh:
-                    self.pulse(index, dist + 1, dim, -1, time + self.m_per_p / self.c_profile[index, dim], R, np.append(rt,R))
+                while dist_in_layer < self.layers[layer, dim] and time_elapsed < self.pulse_width:
+                    time_elapsed += self.time_interval
+                    t_in_layer += self.time_interval
+                    dist_traveled += self.c_profile[layer, dim] * self.time_interval
+                    dist_in_layer += self.c_profile[layer, dim] * self.time_interval
+
+                # Fix for overshoot in distance
+                dist_difference = self.layers[layer, dim] - dist_in_layer
+                dist_traveled += dist_difference
+                dist_in_layer += dist_difference
+                time_difference = dist_difference / self.c_profile[layer, dim]
+                time_elapsed += time_difference
+                t_in_layer += time_difference
+
+                # Compute R and T coefficients and recursive pulse call
+                R = ((self.impedance[layer-1, dim] - self.impedance[layer, dim]) / (self.impedance[layer-1, dim] + self.impedance[layer, dim])) * coeff
+                T = ((2*self.impedance[layer-1, dim]) / (self.impedance[layer-1, dim] + self.impedance[layer, dim])) * coeff
+                self.pulse(layer-1, dim, 1, time_elapsed, 0, dist_traveled, 0, R, np.append(rt,R))
+                if layer+1 < self.layers.shape[0]:
+                    self.pulse(layer+1, dim, -1, time_elapsed, 0, dist_traveled, 0, T, np.append(rt,T))
+
+    def inversion(self):
+        return
 
 
 def random_contour(layers, size):
@@ -126,8 +137,9 @@ def run():
     c = [1500, 1450, 1700, 1900]
     attenuation = [0, 0, 0, 0]
     sample_f = 16.67e3
-    time_interval = 0.0001
-    sbp_simulate = SBP_Simulate_v2(layers, rho, c, attenuation, sample_f, time_interval)
+    time_interval = 0.01
+    sbp_simulate = SBP_Simulate_v2(time, layers, rho, c, attenuation, sample_f, time_interval)
+    sbp_simulate.simulate()
 
     # Plots 
     plt.figure(1)
